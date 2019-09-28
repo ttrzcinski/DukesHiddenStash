80816c20712c4f76bdbb2aa7a11a8de6083afa80
Tomasz_Trzcinski <Tomasz_Trzcinski>
2019-09-28 11:15:55 +0200

    Simplified interfaces.

---
+++ src/org/ttrzcinski/interfaces/IShop.java
@@ -0,0 +1,43 @@
+package org.ttrzcinski.interfaces;
+
+/**
+ * Pattern for creating classes faster with control of it's number.
+ */
+public interface IShop<T> {
+
+  /**
+   * Creates new instance overwriting current instance.
+   *
+   * @return new instance
+   */
+  T createNew();
+
+  /**
+   * Returns the handle to currently kept instance.
+   *
+   * @return current instance
+   */
+  T getCurrent();
+
+  /**
+   * Persists current instance with new unique id in repository.
+   *
+   * @return true, if done, false means errors while saving
+   */
+  boolean saveCurrent();
+
+  /**
+   * Loads the last known instance from repository.
+   *
+   * @return last known instance
+   */
+  T LoadTheNewest();
+
+  /**
+   * Loads the instance with given name.
+   *
+   * @param uniqueName given name
+   * @return instance, if exists, null otherwise
+   */
+  T loadFromRepo(String uniqueName);
+}
--- src/org/ttrzcinski/interfaces/IStringHasValue.java
+++ src/org/ttrzcinski/interfaces/IStringHasValue.java
@@ -3,5 +3,5 @@ package org.ttrzcinski.interfaces;
 @FunctionalInterface
 public interface IStringHasValue {
 
-  public boolean hasValue(String str);
+  boolean hasValue(String str);
 }
