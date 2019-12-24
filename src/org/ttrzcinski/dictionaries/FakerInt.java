1d70e6ff9e86a52780a12ad94dcf4d27899ad696
Tomasz_Trzcinski <Tomasz_Trzcinski>
2019-12-24 16:41:30 +0100

    Added Faker classes for Int and Long.

---
+++ src/org/ttrzcinski/dictionaries/FakerInt.java
@@ -0,0 +1,21 @@
+package org.ttrzcinski.dictionaries;
+
+/**
+ * Set of default values to use in unit tests with int parameters.
+ */
+public class FakerInt {
+
+  private final static int MIN_NEGATIVE = Integer.MIN_VALUE;
+
+  private final static int SOME_NEGATIVE = -2;
+
+  private final static int FIRST_NEGATIVE = -1;
+
+  private final static int ZERO = 0;
+
+  private final static int FIRST_POSITIVE = 1;
+
+  private final static int SOME_POSITIVE = 2;
+
+  private final static int MAX_NEGATIVE = Integer.MAX_VALUE;
+}
---
+++ src/org/ttrzcinski/dictionaries/FakerLong.java
@@ -0,0 +1,21 @@
+package org.ttrzcinski.dictionaries;
+
+/**
+ * Set of default values to use in unit tests with long parameters.
+ */
+public class FakerLong {
+
+  private final static long MIN_NEGATIVE = Long.MIN_VALUE;
+
+  private final static long SOME_NEGATIVE = -2L;
+
+  private final static long FIRST_NEGATIVE = -1L;
+
+  private final static long ZERO = 0L;
+
+  private final static long FIRST_POSITIVE = 1L;
+
+  private final static long SOME_POSITIVE = 2L;
+
+  private final static long MAX_NEGATIVE = Long.MAX_VALUE;
+}
