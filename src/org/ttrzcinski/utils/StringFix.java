7cf9606a58059dc1a3cc0c92bfbc1708ac205325
Tomasz_Trzcinski <Tomasz_Trzcinski>
2019-12-24 14:25:22 +0100

    Split StringFix tests into method unit test files.

--- src/org/ttrzcinski/utils/StringFix.java
+++ src/org/ttrzcinski/utils/StringFix.java
@@ -23,11 +23,7 @@ public final class StringFix {
    * @return fixed string
    */
   public static String toNotNull(final String given) {
-    if (given == null || given.trim().length() == 0) {
-      return "";
-    } else {
-      return given.trim();
-    }
+    return given == null || given.trim().length() == 0 ? "" : given.trim();
   }
 
   /**
@@ -47,13 +43,20 @@ public final class StringFix {
    * @param wantedLength wanted length
    * @return extended string
    */
-  public static String padRight(@NotNull final String given,
-      final int wantedLength) {
-    int wantedCount = wantedLength - given.length();
+  public static String padRight(final String given, final int wantedLength) {
+    String givenFix = "";
+    int wantedCount = 0;
+    if (!ParamCheck.isSet(given)) {
+      givenFix = "";
+    } else {
+      givenFix = given;
+      wantedCount = wantedLength - givenFix.length();
+    }
+    ;
     return wantedLength < 0 || wantedCount < 0
-        ? given
+        ? givenFix
         : String.format("%s%s",
-            given,
+            givenFix,
             " ".repeat(wantedCount)
         );
   }
@@ -65,28 +68,59 @@ public final class StringFix {
    * @param wantedLength wanted length
    * @return extended string
    */
-  public static String padLeft(@NotNull final String given,
-      final int wantedLength) {
-    int wantedCount = wantedLength - given.length();
+  public static String padLeft(@NotNull final String given, final int wantedLength) {
+    String givenFix = "";
+    int wantedCount = 0;
+    if (!ParamCheck.isSet(given)) {
+      givenFix = "";
+    } else {
+      givenFix = given;
+      wantedCount = wantedLength - givenFix.length();
+    }
+    ;
     return wantedLength < 0 || wantedCount < 0
-        ? given
+        ? givenFix
         : String.format("%s%s",
             " ".repeat(wantedCount),
-            given
+            givenFix
         );
   }
 
   /**
-   * Cuts wanted number of chars from the end of string.
+   * Cuts wanted number of characters from the end of string.
    *
    * @param given given string
-   * @param count number of characters to remove
+   * @param endingLength number of characters to remove
    * @return cut-off string
    */
-  public static String cutLast(@NotNull final String given, int count) {
-    int finalCount = given.length() - count;
-    return finalCount >= 0
-        ? given.substring(0, finalCount)
-        : "";
+  public static String cutLast(final String given, int endingLength) {
+    if (!ParamCheck.isSet(given)) {
+      return given;
+    }
+    if (endingLength <= 0) {
+      return given;
+    } else if (endingLength >= given.length()) {
+      return "";
+    }
+    return given.substring(0, given.length() - endingLength);
+  }
+
+  /**
+   * Cuts wanted number of characters from beginning of string.
+   *
+   * @param given given string
+   * @param startingLength number of characters to remove
+   * @return cut-off string
+   */
+  public static String cutFirst(final String given, int startingLength) {
+    if (!ParamCheck.isSet(given)) {
+      return given;
+    }
+    if (startingLength <= 0) {
+      return given;
+    } else if (startingLength >= given.length()) {
+      return "";
+    }
+    return given.substring(startingLength);
   }
 }
---
+++ test/org/ttrzcinski/utils/string_fix/StringFix_simpleTest.java
@@ -0,0 +1,55 @@
+package org.ttrzcinski.utils.string_fix;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.ttrzcinski.dictionaries.FakerString.EMPTY;
+import static org.ttrzcinski.dictionaries.FakerString.EMPTY_TO_TRIM;
+import static org.ttrzcinski.dictionaries.FakerString.NULL;
+import static org.ttrzcinski.dictionaries.FakerString.SOME;
+import static org.ttrzcinski.dictionaries.FakerString.SOME_TO_EMPTY;
+import static org.ttrzcinski.dictionaries.FakerString.SOME_TO_LEFT_TRIM;
+import static org.ttrzcinski.dictionaries.FakerString.SOME_TO_RIGHT_TRIM;
+
+import org.junit.jupiter.api.Test;
+import org.ttrzcinski.utils.StringFix;
+
+class StringFix_simpleTest {
+
+  @Test
+  void simple_someValue() {
+    // Arrange
+    String testObject = SOME_TO_EMPTY;
+    String expected = SOME;
+
+    // Act
+    String result = StringFix.simple(testObject);
+
+    // Assert
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void simple_empty() {
+    // Arrange
+    String testObject = EMPTY_TO_TRIM;
+    String expected = EMPTY;
+
+    // Act
+    String result = StringFix.simple(testObject);
+
+    // Assert
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void simple_null() {
+    // Arrange
+    String testObject = NULL;
+    String expected = EMPTY;
+
+    // Act
+    String result = StringFix.simple(testObject);
+
+    // Assert
+    assertEquals(expected, result);
+  }
+}
