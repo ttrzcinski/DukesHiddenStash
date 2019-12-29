03c53a62ee403a2c05c761270b2d2de755d50627
TOMASZ_TRZCINSKI <Tomasz_Trzcinski@epam.com>
2019-12-29 23:02:54 +0100

    Fixed JavaDoc.

--- src/org/ttrzcinski/interfaces/I2Console.java
+++ src/org/ttrzcinski/interfaces/I2Console.java
@@ -4,6 +4,7 @@ package org.ttrzcinski.interfaces;
  * Forces adding method to show properties n console.
  *
  * @author <a href="mailto:trzcinski.tomasz.1988@gmail.com">Tomasz T.</a>
+ * @version 1.0
  */
 public interface I2Console {
 
