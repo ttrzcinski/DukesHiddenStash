c0f780a8280f0c27b201f066a25d16de755f8dc1
ttrzcinski <trzcinski.tomasz.1988@gmail.com>
2020-03-22 20:12:11 +0100

    Added just because it was missing.

---
+++ src/org/ttrzcinski/dictionaries/FakerBool.java
@@ -0,0 +1,17 @@
+package org.ttrzcinski.dictionaries;
+
+/**
+ * Set of default values to use in unit tests with bool parameters.
+ */
+public class FakerBool {
+
+    private static final boolean POSITIVE = true;
+
+    private static final boolean NEGATIVE = false;
+
+    /**
+     * Hidden constructor.
+     */
+    private FakerBool() {
+    }
+}
