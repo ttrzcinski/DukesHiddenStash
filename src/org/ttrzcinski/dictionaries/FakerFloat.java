0654fa14de5dec81f06990044d6db841c8aef98b
Tomasz Trzciński <trzcinski.tomasz.1988@gmail.com>
2020-04-21 23:30:33 +0200

    Create FakerFloat.java
    Added missing Float Faker dictionary.

---
+++ src/org/ttrzcinski/dictionaries/FakerFloat.java
@@ -0,0 +1,25 @@
+package org.ttrzcinski.dictionaries;
+
+/**
+ * Set of default values to use in unit tests with float parameters.
+ */
+public class FakerFloat {
+
+    private static final float BEFORE_ZERO = -0.01f;
+
+    private static final float ZERO = 0.0f;
+
+    private static final float AFTER_ZERO = 0.01f;
+
+    private static final float BEFORE_ONE = 0.99f;
+
+    private static final float ONE = 1.00f;
+
+    private static final float AFTER_ONE = 1.01f;
+
+    /**
+     * Hidden constructor.
+     */
+    private FakerFloat() {
+    }
+}
